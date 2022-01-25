e868a6ca8e2a30c54dd24ab1516a91ab858b23e1
Gerência Ecommerce Kazanto <felipealmeidacorrea@outlook.com.br>
2022-01-25 17:22:56 -0400

    working on search box

---
+++ files/js/fac-kzt-app.js
@@ -0,0 +1,8 @@
+var $ = $;
+$(".top-bar").slick({
+    autoplay: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    arrows: false
+});
--- files/js/faz-kzt-app.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function(){
-    $('.top-bar').slick({
-      setting-name: setting-value
-    });
-  });
