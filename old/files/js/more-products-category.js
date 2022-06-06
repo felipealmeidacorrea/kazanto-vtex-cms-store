be8ee2e381aa83cb54348abe05b77a8cb6b73dad
Gerência Ecommerce Kazanto <felipealmeidacorrea@outlook.com.br>
2022-06-06 11:10:50 -0400

    removing files from old site

--- old/files/js/more-products-category.js
+++
@@ -1,2 +0,0 @@
-/* DESKTOP-GUOJE28 - 22/03/2021 16:15:37 GMT */
-$(".vitrine[id*=ResultItems]").QD_infinityScroll();
--- old/files/js/store-login.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-$(window).load(function(){
-	$(".vtexIdUI-close, #vtexIdUI-global-loader").click(function(){window.location.href="/"});
-});
